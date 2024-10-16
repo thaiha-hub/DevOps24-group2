ff317052232c0a8599f31b65894d7df2b8202d23
SvartaStorken <martin_junkan@hotmail.com>
2024-10-16 19:13:02 +0200

    Update Group2.py
    The question is added

--- Group2.py
+++ Group2.py
@@ -1,2 +1,5 @@
 print("Hello Welcome to Lernia DevOps24 Search tool")
 print("Please enter a group number")
+group = int(input())
+if group == 2:
+    print ("Wow good pic group 2 was the best in the class")
