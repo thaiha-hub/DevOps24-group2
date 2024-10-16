ae2fb870621cc0ed9121bd7646e387fdadde4598
SvartaStorken <martin_junkan@hotmail.com>
2024-10-16 21:47:09 +0200

    Update Group2.py
    Adding group minimum

--- Group2.py
+++ Group2.py
@@ -5,4 +5,6 @@ if group == 2:
     print ("Wow good pic group 2 was the best in the class")
 
 elif group > 6:
-    print("There are only six groups, try to search for group 2 it is legendary")
+    print("There are only six groups, try to search for group 2 it is legendary!")
+elif group < 0:
+    print("The group count starts at 1, try to search for group 2 it is legendary!")
