65345295e01260413064f7227bef5ac52be6ab12
thaiha-hub <devops.engineer0528@gmail.com>
2024-10-17 09:32:40 +0300

    Update Group2.py
    Fix bugs

--- Group2.py
+++ Group2.py
@@ -1,11 +1,10 @@
 print("Hello Welcome to Lernia DevOps24 Search tool")
-print("Please enter a group number")
-group = int(input())
+group = input("Please enter a group number: )
 if group == 2:
     print ("Wow good pic group 2 was the best in the class")
-
 elif group > 6:
     print("There are only six groups, try to search for group 2 it is legendary!")
 elif group < 0:
     print("The group count starts at 1, try to search for group 2 it is legendary!")
-else print("Sorry there is no information to be found about group" + group)
+else:
+    print("Sorry there is no information to be found about group" + group)
