a76277e954eb208ff05cbd8843b7765c7c609ba2
qmannen <151250512+qmannen@users.noreply.github.com>
2024-10-17 13:01:04 +0200

    Update Group2.py
    added shitposts for grupp 3 och 4

--- Group2.py
+++ Group2.py
@@ -4,9 +4,17 @@ group = int(input())
 if group == 2:
     print ("Wow good pic group 2 was the best in the class")
 
+if group == 3:
+    print ("Very bad group 3 is bad in the class")
+
+if group == 4:
+    print ("ah hell naws")
+
+
+
 elif group > 6:
     print("There are only six groups, try to search for group 2 it is legendary!")
-elif group < 0:
+elif group < 1:
     print("The group count starts at 1, try to search for group 2 it is legendary!")
 else: 
     print("Sorry there is no information to be found about group " + str(group))
