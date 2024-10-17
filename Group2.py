81da67f3fe1843ce8fa14a4f711dcbf7f63e96a8
qmannen <151250512+qmannen@users.noreply.github.com>
2024-10-17 13:09:28 +0200

    Update Group2.py

--- Group2.py
+++ Group2.py
@@ -9,12 +9,3 @@ if group == 3:
 
 if group == 4:
     print ("ah hell naws")
-
-
-
-elif group > 6:
-    print("There are only six groups, try to search for group 2 it is legendary!")
-elif group < 1:
-    print("The group count starts at 1, try to search for group 2 it is legendary!")
-else: 
-    print("Sorry there is no information to be found about group " + str(group))
