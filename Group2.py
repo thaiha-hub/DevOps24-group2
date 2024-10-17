15724986dd492d494378efb1ae68141802e0cff3
qmannen <151250512+qmannen@users.noreply.github.com>
2024-10-17 18:11:35 +0200

    Update Group2.py
    Added ! and a special option for group 5

--- Group2.py
+++ Group2.py
@@ -1,11 +1,16 @@
-print("Hello Welcome to Lernia DevOps24 Search tool!")
+print("Hello Welcome to Lernia DevOps24 Search tool")
 print("Please enter a group number")  #
 group = int(input())
 if group == 2:
     print ("Wow good pic group 2 was the best in the class")
 
-if group == 3:
-    print ("Very bad group 3 is bad in the class")
+if group == 5:
+    print ("5 is not a creative number")
 
-if group == 4:
-    print ("ah hell naws")
+
+elif group > 5:
+    print("There are only six groups, try to search for group 2 it is legendary!")
+elif group < 0:
+    print("The group count starts at 1, try to search for group 2 it is legendary!")
+else: 
+    print("Sorry there is no information to be found about group " + str(group))
